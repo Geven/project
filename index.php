b01ecc4597ad117490db0f07d19b467b2119be91
Oleg Buchnev <oleg.buchnev@createch-group.com>
2015-04-06 18:19:30 +0300

    The first commit to a html/css development

---
+++ index.php
@@ -0,0 +1,76 @@
+﻿<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+
+    <title>Товары</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script src="//code.jquery.com/jquery.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <script type="text/javascript" src="./js/main.js"></script>
+
+</head>
+
+<body>
+<div class="searchForm">
+    <a class = "searchValueLink" href="#"><div id="searchValue">
+        <div id="searchIcon">
+                <div id="menuElements">ТВ, Аудио, Видео</div><div class="arrowUp"></div>
+                <div id="hiddenElements">
+                <a class="elementsLinks" href="#">Везде</a>
+                <a class="elementsLinks" href="#">Распродажа товаров</a>
+                <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                    <a class="elementsLinks" href="#">Распродажа товаров</a>
+                </div>
+
+        </div>
+            <div><input type="text" class="searchInputBar" /></div>
+</div></a>
+<button class="searchButton">Найти</button>
+</div>
+
+<div class="navbar">
+    <div class="textNavBar">
+            <div class="sortText">Сортировать по</div>
+            <a class="sortValuesLink" href="#"><div id="sortValues">популярности</div><div class="arrow"></div>
+            <div id="hiddenSearchValues">
+                <a class="elementsLinks" href="#">цене, сначала недорогие</a>
+                <a class="elementsLinks" href="#">цене, сначала дорогие</a>
+                <a class="elementsLinks" href="#">названию</a>
+                <a class="elementsLinks" href="#">новизне</a>
+                <a class="elementsLinks" href="#">рейтингу</a>
+                <a class="elementsLinks" href="#">популярности</a>
+
+            </div>
+    </div></a>
+    <div class="catalogView">
+        Вид каталога
+        </div>
+    <button class="buttonIcon" title="Плиткой"></button>
+    <button class="buttonIconSecond" title="Список с фото"></button>
+    <button class="buttonIconThird" title="Список без фото"></button>
+</div>
+    <div class="tabletList">
+
+    </div>
+
+</body>
+
+</html>
---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#searchIcon').click(function() {
+        $('#hiddenElements').slideToggle("fast");
+    });
+    $('#sortValues').click(function() {
+        $('#hiddenSearchValues').slideToggle("fast");
+    });
+
+});
+
